abfca31dc2dd74931a7a168540ea3fca2007615a
enoklenok <sinyaenok@gmail.com>
2025-07-19 13:23:03 +0900

    feat: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  resolve: {
+    alias: [
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@constants', replacement: '/src/constants' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@layouts', replacement: '/src/layouts' },
+      { find: '@stores', replacement: '/src/stores' },
+      { find: '@styles', replacement: '/src/styles' },
+      { find: '@utils', replacement: '/src/utils' },
+    ],
+  },
 })
